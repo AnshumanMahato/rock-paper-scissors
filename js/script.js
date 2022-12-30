c34a3c7dd1ede8615da64619a407d7439c2ed99a
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-12-30 22:17:59 +0530

    logic done

--- js/script.js
+++ js/script.js
@@ -20,6 +20,9 @@ const selectionWindow = document.querySelector('.selection-window');
 const resultWindow = document.querySelector('.result-window');
 const playerChip = resultWindow.querySelector('.choice__player>.chip');
 const compChip = resultWindow.querySelector('.choice__opponent>.chip');
+const displayResult = resultWindow.querySelector('.result');
+const resultText = displayResult.querySelector('span');
+const btnPlayAgain = displayResult.querySelector('.btn--play-again');
 
 selectionWindow.addEventListener('click', async (e) => {
   if (!e.target.classList.contains('chip')) return;
@@ -27,10 +30,10 @@ selectionWindow.addEventListener('click', async (e) => {
 
   //reset screens
   selectionWindow.classList.toggle('hide');
-  if (game.playerChoice)
-    playerChip.classList.remove(`chip--${game.playerChoice}`);
-  if (game.opponentChoice)
-    compChip.classList.remove(`chip--${game.opponentChoice}`);
+  if (game.playerChoice !== undefined)
+    playerChip.classList.remove(`chip--${gameOptions[game.playerChoice]}`);
+  if (game.opponentChoice !== undefined)
+    compChip.classList.remove(`chip--${gameOptions[game.opponentChoice]}`);
 
   //Get user selection
   game.playerChoice = parseInt(e.target.dataset.option);
@@ -43,19 +46,34 @@ selectionWindow.addEventListener('click', async (e) => {
   compChip.classList.add(`chip--${gameOptions[game.opponentChoice]}`);
 
   //Evaluate
-
   if (game.playerChoice === game.opponentChoice) {
-    console.log('draw');
+    resultText.textContent = 'DRAW';
   } else if (
     (game.playerChoice + 1) % 5 === game.opponentChoice ||
     (game.playerChoice + 3) % 5 === game.opponentChoice
   ) {
-    console.log('player win');
+    resultText.textContent = 'YOU WIN';
+    game.score++;
   } else {
-    console.log('player lose');
+    resultText.textContent = 'YOU LOSE';
+    game.score--;
   }
+
+  //Display Result
+  await wait(1);
+  scoreElememt.textContent = game.score;
+  resultWindow.classList.remove('result-pending');
+  resultWindow.classList.add('result-declared');
+  displayResult.classList.toggle('hide');
 });
 
+btnPlayAgain.addEventListener('click', (e) => {
+  displayResult.classList.toggle('hide');
+  resultWindow.classList.add('result-pending');
+  resultWindow.classList.remove('result-declared');
+  resultWindow.classList.toggle('hide');
+  selectionWindow.classList.toggle('hide');
+});
 /* Get the documentElement (<html>) to display the page in fullscreen */
 var elem = document.documentElement;
 
