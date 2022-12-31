87c7e3968fa238073c40271222ce4a23c20915e5
AnshumanMahato <58422570+AnshumanMahato@users.noreply.github.com>
2022-12-31 15:01:25 +0530

    fusll site done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 //jshint esversion:8
 const game = {
   playerChoice: undefined,
-  opponentChoice: undefined,
+  compChoice: undefined,
+  currWinner: undefined,
   score: 0,
 };
 
@@ -32,8 +33,8 @@ selectionWindow.addEventListener('click', async (e) => {
   selectionWindow.classList.toggle('hide');
   if (game.playerChoice !== undefined)
     playerChip.classList.remove(`chip--${gameOptions[game.playerChoice]}`);
-  if (game.opponentChoice !== undefined)
-    compChip.classList.remove(`chip--${gameOptions[game.opponentChoice]}`);
+  if (game.compChoice !== undefined)
+    compChip.classList.remove(`chip--${gameOptions[game.compChoice]}`);
 
   //Get user selection
   game.playerChoice = parseInt(e.target.dataset.option);
@@ -42,21 +43,24 @@ selectionWindow.addEventListener('click', async (e) => {
 
   await wait(1);
   //genrerate a random opponent selection
-  game.opponentChoice = Math.floor(Math.random() * 5);
-  compChip.classList.add(`chip--${gameOptions[game.opponentChoice]}`);
+  game.compChoice = Math.floor(Math.random() * 5);
+  compChip.classList.add(`chip--${gameOptions[game.compChoice]}`);
 
   //Evaluate
-  if (game.playerChoice === game.opponentChoice) {
+  if (game.playerChoice === game.compChoice) {
     resultText.textContent = 'DRAW';
+    game.currWinner = undefined;
   } else if (
-    (game.playerChoice + 1) % 5 === game.opponentChoice ||
-    (game.playerChoice + 3) % 5 === game.opponentChoice
+    (game.playerChoice + 1) % 5 === game.compChoice ||
+    (game.playerChoice + 3) % 5 === game.compChoice
   ) {
     resultText.textContent = 'YOU WIN';
     game.score++;
+    game.currWinner = 'player';
   } else {
     resultText.textContent = 'YOU LOSE';
     game.score--;
+    game.currWinner = 'comp';
   }
 
   //Display Result
@@ -65,6 +69,13 @@ selectionWindow.addEventListener('click', async (e) => {
   resultWindow.classList.remove('result-pending');
   resultWindow.classList.add('result-declared');
   displayResult.classList.toggle('hide');
+
+  if (game.currWinner === 'player') {
+    playerChip.querySelector('.winner').classList.remove('hide');
+  }
+  if (game.currWinner === 'comp') {
+    compChip.querySelector('.winner').classList.remove('hide');
+  }
 });
 
 btnPlayAgain.addEventListener('click', (e) => {
@@ -73,6 +84,12 @@ btnPlayAgain.addEventListener('click', (e) => {
   resultWindow.classList.remove('result-declared');
   resultWindow.classList.toggle('hide');
   selectionWindow.classList.toggle('hide');
+  if (game.currWinner === 'player') {
+    playerChip.querySelector('.winner').classList.add('hide');
+  }
+  if (game.currWinner === 'comp') {
+    compChip.querySelector('.winner').classList.add('hide');
+  }
 });
 /* Get the documentElement (<html>) to display the page in fullscreen */
 var elem = document.documentElement;
